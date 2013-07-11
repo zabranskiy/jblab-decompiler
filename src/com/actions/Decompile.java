f8fae985c9c0f3beb652763f8c0dd3e8f9ae55f1
Dmitriy.Zabranskiy <dmitriy.zabranskiy@gmail.com>
2013-07-11 13:12:20 +0400

    first version that works

--- src/com/actions/Decompile.java
+++ src/com/actions/Decompile.java
@@ -2,67 +2,69 @@ package com.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.sdc.DecompilerService;
-import sun.misc.IOUtils;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.testFramework.LightVirtualFile;
+import com.sdc.java.JavaClassVisitor;
+import org.objectweb.asm.ClassReader;
 
 import java.io.DataInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Action for triggering decompilation.
+ * Dmitriy Zabranskiy, 2013
+ */
+
 public class Decompile extends AnAction {
 
-/*    private String getFileExtension(final DataContext dataContext) {
-        final VirtualFile file = DataKeys.VIRTUAL_FILE.getData(dataContext);
-        return file == null ? null : file.getExtension();
-    }*/
-
-/*    @Override
-    public void update(AnActionEvent e) {
-        super.update(e);
-
-        final String extension = getFileExtension(e.getDataContext());
-        this.getTemplatePresentation().setEnabled(extension != null && "class".equals(extension));
-    }*/
-
     public void actionPerformed(AnActionEvent e) {
-        DataContext dataContext = e.getDataContext();
-        Project project = DataKeys.PROJECT.getData(dataContext);
-        VirtualFile virtualFile = DataKeys.VIRTUAL_FILE.getData(dataContext);
-        assert virtualFile != null;
-        VirtualFile classRootForFile = ProjectRootManager.getInstance(project).getFileIndex().getClassRootForFile(virtualFile);
-
-        String path = virtualFile.getPresentableName();
-
-
-        String filePath = virtualFile.getPath();
-        String basePath = classRootForFile.getPresentableUrl();
-        String internalClassName = filePath.substring(filePath.indexOf('!') + 2, filePath.length());
-
-
-        try {
-//            InputStream in = virtualFile.getInputStream();
-            InputStream in = this.getClass().getClassLoader().getResourceAsStream(internalClassName);
-            DataInputStream data = new DataInputStream(in);
-            int magic = data.readInt();
-            if (magic == 0xCAFEBABE) {
-                DecompilerService decompilerService = new DecompilerService();
-                decompilerService.decompile(in);
+        final Project currentProject = e.getData(PlatformDataKeys.PROJECT);
+        final VirtualFile virtualFile = DataKeys.VIRTUAL_FILE.getData(e.getDataContext());
+        if (virtualFile != null && !virtualFile.isDirectory()) {
+            try {
+                assert currentProject != null;
+                // Detection class file with any file extension
+                DataInputStream data = new DataInputStream(virtualFile.getInputStream());
+                final int magic = data.readInt();
+                if (magic == 0xCAFEBABE) {
+                    // TODO: how to reset InputStream? don't call virtualFile.getInputStream() second time
+                    final LightVirtualFile file = new LightVirtualFile(virtualFile.getNameWithoutExtension() + ".java", decompile(virtualFile.getInputStream()));
+                    final PsiFile psiFile = PsiManager.getInstance(currentProject).findFile(file);
+                    // Reformat decompiled code by IDEA
+                    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                        @Override
+                        public void run() {
+                            assert psiFile != null;
+                            CodeStyleManager.getInstance(currentProject).reformat(psiFile);
+                        }
+                    });
+                    FileEditorManager.getInstance(currentProject).openFile(file, true);
+                }
+                data.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
             }
-        } catch (IOException e1) {
-            e1.printStackTrace();
         }
+    }
 
+    public String decompile(InputStream is) throws IOException {
+        ClassReader cr = new ClassReader(is);
+        // params
+        final int tabSize = 4;
+        final int textWidth = 100;
 
-//        URL url = new URL(classRootForFile.getPresentableUrl());
-//        InputStream is = url.openStream();
-//        decompilerService.decompile(is);
-
+        JavaClassVisitor cv = new JavaClassVisitor(textWidth, tabSize);
+        cr.accept(cv, 0);
+        return cv.getDecompiledCode();
     }
 }
 
